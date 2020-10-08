48b77d91b7d57370e1a64a79cbd005ec2e4113ec
P21adawson <70960562+P21adawson@users.noreply.github.com>
2020-10-07 22:52:55 -0400

    Create hw4-1-2.py

---
+++ hw4-1-2.py
@@ -0,0 +1,11 @@
+# Azaan Dawson Amdg 10/7/20
+
+x = input("Value of Card 1")
+y = input("Value of Card 2")
+
+total_score = int(x)+  int(y)
+
+if total_score <= 21 :
+  print("safe")
+else :
+  print("bust")
